a40546a79b02c5d6efcc47b8694cf4574216bceb
mcasals <mcasals@fie.undef.edu.ar>
2025-09-18 20:17:20 -0300

    agregue la parte de presentacion

--- cvsite/urls.py
+++ cvsite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('portfolio.urls')),
+    path('admin/', admin.site.urls), #es la direccion a mi administrador
+    path('mauri/', include('portfolio.urls')),
+    path('', include('presentacion.urls')),
 ]
