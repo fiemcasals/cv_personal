9df5fc2bc6d2c69e884e1c49b424d1a25616923c
Maia <guascomaia@gmail.com>
2025-10-09 23:54:54 -0300

    Delete cvsite directory

--- cvsite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls), #es la direccion a mi administrador
-    path('mauri/', include('portfolio.urls')),
-    path('', include('presentacion.urls')),
-]
